e1020441b512d7eb1a8eb6cf3ad4812a3b53fe39
Joseph <jtawil2015@gmail.com>
2020-09-02 19:15:21 -0800

    added dequeue function

--- queue.js
+++ queue.js
@@ -12,7 +12,20 @@ function queueFunction(){
         return data[beg];
     }
 
-    return {enqueue, peek, data};
+    function dequeue(){
+        if(count != beg){
+            const entry = data[beg];
+            delete data[beg];
+            beg++;
+            return entry;
+        }
+        else{
+            return "Queue is empty";
+        }
+
+
+    }
+    return {enqueue, peek, dequeue, data};
 }
 
 const queue = queueFunction();
@@ -20,5 +33,6 @@ const queue = queueFunction();
 queue.enqueue(1);
 queue.enqueue(2);
 queue.enqueue(3);
+queue.dequeue();
 
 console.log(queue.peek());
